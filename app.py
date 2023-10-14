e7ae3d0a8e4a985dae1c00d8c880867d839ee802
Ananya Aggarwal <60582678+AnanyaAgg@users.noreply.github.com>
2023-10-14 08:10:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,14 @@ import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
 st.title('my classification app')
-page_bg_img = '''
+st.markdown ('''
 <style>
-body {
+.stApp {
 background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
 background-size: cover;
 }
 </style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
+''', unsafe_allow_html=True)
 
 @st.cache_resource
 def my_model():
