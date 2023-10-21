a8e7d86754985d91939d05caf8151c4f38164dfe
Ananya Aggarwal <60582678+AnanyaAgg@users.noreply.github.com>
2023-10-21 00:13:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
-st.title('my classification app')
-st.markdown (f'''
+import streamlit as st
+import numpy as np
+
+tab1, tab2, tab3 = st.tabs(["Home", "Demo", "About Me"])
+data = np.random.randn(10, 1)
+
+tab1.subheader("What is AIML?")
+tab1.write("AIML stands for artificial inteligence machine learning. Essentially, AIML is the process of teaching a computer to problem solve and to replicate the human brain's logic to create its own knowledge after being trained with a dataset.")
+
+tab2.subheader("Try the model!")
+tab2.markdown (f'''
 <style>
 .stApp {{
 background-image: url("https://images.unsplash.com/photo-1697222691126-c1be7bde3ac5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1248&q=80");
@@ -29,11 +38,13 @@ if img is not None:
 	col = st.columns(2)
 	with col[0]:
 		for i in temp:
-			st.write(res[0].names[i])
+			tab2.write(res[0].names[i])
 	with col[1]:
 		for i in conf:
-			st.write(str(i))
-	st.success('congrats!')
+			tab2.write(str(i))
+	tab2.success('congrats!')
+tab3.subheader("About Me!")
+tab3.write("Hi! I am a junior in High School and I live in the Bay Area.")
 
 
 
