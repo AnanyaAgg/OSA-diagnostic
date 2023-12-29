100ee75d58561990e4e0e4d8dd06c5a2bd7279cd
Ananya Aggarwal <60582678+AnanyaAgg@users.noreply.github.com>
2023-12-28 20:16:22 -0800

    Update app.py

--- app.py
+++ app.py
@@ -29,9 +29,14 @@ with tab4:
 	col2 = st.columns(2)
 	with col2[0]:
 		st.write("contact us here!")
+		st.markdown("[![Click me](icon.jpeg)](https://www.instagram.com/ananya._.aggarwal/)")
 		st.link_button("", "https://www.instagram.com/ananya._.aggarwal/")
 	with col2[1]:
 		st.write("learn more about me...")
+
+
+
+    
 
 
 
