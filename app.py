f3d4cf9e070048052690f368179028eb188708c0
Ananya Aggarwal <60582678+AnanyaAgg@users.noreply.github.com>
2024-01-18 19:43:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ with tab2:
 	st.header("Get your AI-backed diagnosis today!")
 	'''general questions'''
 	with st.expander("What is your age?"):
-		genre = st.radio(["<20", "20-30", "30-40", "40-50", "50-60", ">60"])
+		genre = st.radio("", ["<20", "20-30", "30-40", "40-50", "50-60", ">60"])
 	with st.expander():
 		genre = st.radio("What is your gender by birth?", ["male", "female", "prefer not to say"])
 	with st.expander():
